628bea2cd67afee91ff78984a3dc5c926bfa8de7
Shannon Riester <shannonriester@gmail.com>
2016-06-29 13:16:36 -0500

    have good starting with css, html and beginnings of js

---
+++ main.js
@@ -0,0 +1,6 @@
+var $ulFiles = $('#files');
+var $liFiles = $('<li>Dropbox</li><li>Skydrive</li><li>FTP Server</li><li>Dropbox</li><li>Push</li>');
+
+
+//this makes the <li>s prepend to only the first ul
+  $ulFiles.append($liFiles);
